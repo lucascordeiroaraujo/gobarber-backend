0a7c77fe062eedc5081dfc622ae14f568f346327
Lucas Cordeiro Araujo <lucascordeiroaraujo@gmail.com>
2021-01-24 01:56:48 -0300

    :recycle: rules for creating a new schedule

--- src/modules/appointments/services/CreateAppointmentsService.ts
+++ src/modules/appointments/services/CreateAppointmentsService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns'
+import { startOfHour, isBefore, getHours } from 'date-fns'
 
 import { injectable, inject } from 'tsyringe'
 
@@ -27,6 +27,18 @@ export default class CreateAppointmentsService {
     date,
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date)
+
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError(`You can't create an appointment on a past date.`)
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError(`You can't create an appointment with yourself.`)
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError('You can only create appontments between 8am and 5pm.')
+    }
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
--- src/modules/appointments/services/__tests__/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/__tests__/ListProviderDayAvailabilityService.spec.ts
@@ -17,14 +17,14 @@ describe('ListProviderDayAvailability', () => {
 
   it('should be able to list the day availability from provider', async () => {
     await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      user_id: 'user',
+      provider_id: 'provider-id',
+      user_id: 'user-id',
       date: new Date(2021, 4, 20, 14, 0, 0),
     })
 
     await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      user_id: 'user',
+      provider_id: 'provider-id',
+      user_id: 'user-id',
       date: new Date(2021, 4, 20, 15, 0, 0),
     })
 
@@ -33,7 +33,7 @@ describe('ListProviderDayAvailability', () => {
     })
 
     const availability = await listProviderDayAvailability.execute({
-      provider_id: 'user',
+      provider_id: 'provider-id',
       year: 2021,
       month: 5,
       day: 20,
